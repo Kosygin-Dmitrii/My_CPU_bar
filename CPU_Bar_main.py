d3ee57c2ecaa5740d77ff7251b7166dede08c587
Kosygin <Kosygin-Dmitrii@yandex.ru>
2021-02-02 20:44:30 +0300

    step 5 Removing unnecessary options and reviews added by PyCharm

--- CPU_Bar_main.py
+++ CPU_Bar_main.py
@@ -1,131 +1,124 @@
 import tkinter as tk
-from tkinter import ttk         # Моудль для нового вида виджетов с оформлением WINDOW
+from tkinter import ttk                                        # Моудль для нового вида виджетов с оформлением WINDOW
 import sys
 from proccess import CpuBar
-from widget_update import Configure_widgets
+from widget_update import ConfigureWidgets
 
+geom = ''
 
-
-geom = '400x400'
-class Application(tk.Tk, Configure_widgets):           #Наследуемся от класса создания основного окна
-    def __init__(self):             # Перегружаем метод инит у класса
+class Application(tk.Tk, ConfigureWidgets):                    # Наследуемся от класса создания основного окна
+    def __init__(self):                                        # Перегружаем метод инит у класса
         tk.Tk.__init__(self)
-        self.geometry(geom)            # Создание геометрии окна  400x400+50+1000 - ширина высота и отступы от угла
-        self.attributes('-alpha', 1)            # Прозрачнотсь
+        self.geometry("")                                      # Создание геометрии окна  400x400+50+1000 - ширина высота и отступы от угла
+        self.attributes('-alpha', 1)                           # Прозрачнотсь
+        self.attributes('-topmost', True)                      # Поверх всех окон или нет
+        self.overrideredirect(False)                           # Если тру, то удаляет всю рамку виджета(закрыть свернуть)
+        self.resizable(False, False)                           # При фолз запрещает изменять размерность окна
+        self.title('My_CPU_BAR')                               # Панель названия окна
 
-        self.attributes('-topmost', True)       # Поверх всех окон или нет
-        self.overrideredirect(False)         #Если тру, то удаляет всю рамку виджета(закрыть свернуть)
-        self.resizable(False, False)        # При фолз запрещает изменять размерность окна
-        self.title('My_CPU_BAR')           #Панель названия окна
-
-
-        self.cpu = CpuBar()         #Определяем метод вызывающий класс,отражающий ядра, созданный в отдельном модуле.
+        self.cpu = CpuBar()                                    # Определяем метод вызывающий класс,отражающий ядра, созданный в отдельном модуле.
         self.run_set_ui()
 
-
     def run_set_ui(self):
-        self.set_ui()               #Инициализируем окно и запускаем свой метод, который заполнит окно нашими параметрами
-        self.make_bar_cpu_usage()   #Заполняет окно виджетами
-        self.congifure_cpu_bar()    # Конфигурирует значения в ввиджеты, т.к. наследовался класс приложение от виджет_апдейт, то берем от туда этот метод
+        self.set_ui()                                          # Инициализируем окно и запускаем свой метод, который заполнит окно нашими параметрами
+        self.make_bar_cpu_usage()                              # Заполняет окно виджетами
+        self.congifure_cpu_bar()                               # Конфигурирует значения в ввиджеты, т.к. наследовался класс приложение от виджет_апдейт, то берем от туда этот метод
 
-
-    def set_ui(self):               # Набор графических ВИДЖЕТОВ для интерфейса на главном окне
-        exit_but = ttk.Button(self, text='Exit', command=self.app_exit)        #Создание кнопки с названием,  command- команда иполняемая по нажатию, это метод класса с названием app_exit
-        exit_but.pack(fill=tk.X)                #метод - УПАКОВЩИК pack()-сверху по центру (grid, place)  fill(заполнение) растягивает по Х
-
-        self.bar2 = ttk.LabelFrame(self, text='manual')                   # Frame -Class - создание рамки для размещения виджетов
+    def set_ui(self):                                          # Набор графических ВИДЖЕТОВ для интерфейса на главном окне
+        exit_but = ttk.Button(self,                            # Создание кнопки с названием,  command- команда иполняемая по нажатию, это метод класса с названием app_exit
+                              text='Exit',
+                              command=self.app_exit)
+        exit_but.pack(fill=tk.X)                               # Метод - УПАКОВЩИК pack()-сверху по центру (grid, place)  fill(заполнение) растягивает по Х
+        self.bar2 = ttk.LabelFrame(self, text='manual')        # Frame -Class - создание рамки для размещения виджетов
         self.bar2.pack(fill=tk.X)
 
-        self.combo_win = ttk.Combobox(self.bar2, value=['hide',"don't hide",'min'],state='readonly',width=9)    # Combobox-выпадающий список размещаем на фрейме бар2, передаем значения из выпадающего меню и только для чтения в список
-        self.combo_win.current(1)    # Выбираем значение из выпадающего меню по умолчанию (текущее) из списка value по индексу
+        self.combo_win = ttk.Combobox(self.bar2,
+                                      value=['hide',"don't hide",'min'],
+                                      state='readonly',
+                                      width=9)                 # Combobox-выпадающий список размещаем на фрейме бар2, передаем значения из выпадающего меню и только для чтения в список
+        self.combo_win.current(1)                              # Выбираем значение из выпадающего меню по умолчанию (текущее) из списка value по индексу
         self.combo_win.pack(side=tk.LEFT)
 
-
-
-        ttk.Button(self.bar2, text='Move', command=self.configure_win).pack(side=tk.LEFT)              # Создаем кнопку, размещаем на фрейме бар2, side - сторона
-        ttk.Button(self.bar2, text='>>>').pack(side=tk.LEFT)
+        ttk.Button(self.bar2, text='>>>').pack(side=tk.LEFT)   # Создаем кнопку, размещаем на фрейме бар2, side - сторона
 
         self.bar1 = ttk.LabelFrame(self, text='Power')
-        self.bar1.pack(fill=tk.BOTH)            #выравнивание по ширине
+        self.bar1.pack(fill=tk.BOTH)                           # Ввыравнивание по ширине
+
         self.test = ttk.Button(self.bar2, text='test').pack(side=tk.LEFT)
 
+        self.bind_class('Tk', '<Enter>',self.enter_mouse)      # Позволяет сделать окно появляющимся bind - обрабатывает события наведения нажатия и прочее. При наведение на класс-Tk. Будут вызываться методы
+        self.bind_class('Tk', '<Leave>',self.leave_mouse)      # Enter - при наведении мыши Leave - при покидании
 
-        self.bind_class('Tk', '<Enter>',self.enter_mouse)     #Позволяет сделать окно появляющимся bind - jбрабатывает события наведения нажатия и прочее. При наведение на класс-Tk. Будут вызываться методы
-        self.bind_class('Tk', '<Leave>',self.leave_mouse)     #Enter - при наведении мыши Leave - при покидании
-
-        self.combo_win.bind('<<ComboboxSelected>>',self.choise_combo)    #При выборе пунктов используем метод сомбо чойз
+        self.combo_win.bind('<<ComboboxSelected>>',
+                            self.choise_combo)                 # При выборе пунктов используем метод сомбо чойз
 
     def make_bar_cpu_usage(self):
-        ttk.Label(self.bar1, text=f'physical_cores: {self.cpu.cpu_count}, logical cores:{self.cpu.cpu_count_logical}',       #lable  - метка(название, шапка) на фрейме Power
-                  anchor=tk.CENTER).pack(fill=tk.X)     #anchor - якорь прикрепляем как метку к ередине экрана и устанавливаем по ширине
+        ttk.Label(self.bar1,
+                  text=f'physical_cores: {self.cpu.cpu_count}, logical cores:{self.cpu.cpu_count_logical}',             #lable  - метка(название, шапка) на фрейме Power
+                  anchor=tk.CENTER).pack(fill=tk.X)            # anchor - якорь прикрепляем как метку к ередине экрана и устанавливаем по ширине
 
-        self.list_label = []    #
-        self.list_pbar = []     #Прогресс бар
+        self.list_label = []                                   # Имя, вставлен Label, который будет заполнен значением методом configure
+        self.list_pbar = []                                    # Прогресс бар,расширен методом Progressbar,заполнено значением методом configure
 
-        for i in range(self.cpu.cpu_count_logical):             #по количеству потоков создаем списки
+        for i in range(self.cpu.cpu_count_logical):            # По количеству потоков создаем списки
             self.list_label.append(ttk.Label(self.bar1, anchor=tk.CENTER))
-            self.list_pbar.append(ttk.Progressbar(self.bar1, length=100))       #длина будет разбита на 100 делений (под % загрузку процессора)
+            self.list_pbar.append(ttk.Progressbar(self.bar1, length=100))  #длина будет разбита на 100 делений (под % загрузку процессора)
 
         for i in range(self.cpu.cpu_count_logical):
             self.list_label[i].pack(fill=tk.X)
             self.list_pbar[i].pack(fill=tk.X)
 
-        self.ram_lab = ttk.Label(self.bar1,  anchor=tk.CENTER)           #создаем метку для оперативной памяти по центру
-        self.ram_lab.pack(fill=tk.X)                                                #Заполняем растягиваем по Х
-        self.ram_bar = ttk.Progressbar(self.bar1, length=100)                       #Создаем шкалу на 100 делений
-        self.ram_bar.pack(fill=tk.X)                                                #Растягиваем по Х
+        self.ram_lab = ttk.Label(self.bar1,  anchor=tk.CENTER)  # Создаем метку для оперативной памяти по центру
+        self.ram_lab.pack(fill=tk.X)                            # Заполняем растягиваем по Х
+        self.ram_bar = ttk.Progressbar(self.bar1, length=100)   # Создаем шкалу на 100 делений
+        self.ram_bar.pack(fill=tk.X)                            # Растягиваем по Х
 
-    def make_minimal_win(self):                                             # Заполнение окна виджетами в минимальном режиме
+    def make_minimal_win(self):                                 # Заполнение окна виджетами в минимальном режиме
         self.bar_one = ttk.Progressbar(self, length=100)
         self.bar_one.pack(side=tk.LEFT)
-
         self.ram_bar = ttk.Progressbar(self, length=100)
         self.ram_bar.pack(side=tk.LEFT)
-
         ttk.Button(self, text='full', command=self.make_full_win,width=5).pack(side=tk.LEFT)
-        ttk.Button(self, text='move', command=self.configure_win, width=5).pack(side=tk.LEFT)
-
         self.geometry('')
-
         self.update()
         self.configure_minimal_win()
 
 
-    def enter_mouse(self,event):     #Метод при наведении мыши, event - обязательно указатьб при срабатвывании оброботчика ошибок
-        if self.combo_win.current() == 0 or 1: #Если выбрано значение из списка скрывать или не скрывать
-            self.geometry(geom)     #Геометрия окна будет оставаться по умолчанию, такая какая есть
+    def enter_mouse(self,event):                                # Метод при наведении мыши, event - обязательно указатьб при срабатвывании оброботчика ошибок
+        if self.combo_win.current() == 0 or 1:                  # Если выбрано значение из списка скрывать или не скрывать
+            self.geometry(geom)                                 # Геометрия окна будет оставаться по умолчанию, такая какая есть
 
 
     def leave_mouse(self,event):
         if self.combo_win.current() == 0:
-           self.geometry(f'{self.winfo_width()}x20')   # получаем текущее значение размеров окна и меняем его, если выбран пункт 1 - свернуть приложение
+           self.geometry(f'{self.winfo_width()}x28')            # Получаем текущее значение размеров окна и меняем его, если выбран пункт 1 - свернуть приложение
 
 
-    def choise_combo(self, event):                 #Срабатывает при выборе любого метода из бокса
-        if self.combo_win.current() == 2:   # Выбран пункт min
-            self.enter_mouse('')            # Пустая строка нужна чтобы в евент что-то попало(он обязателен)
-            self.unbind_class('Tk','<Enter>')   #отвязываем событие при наведении мыши на окно
+    def choise_combo(self, event):                              # Срабатывает при выборе любого метода из бокса
+        if self.combo_win.current() == 2:                       # Выбран пункт min
+            self.enter_mouse('')                                # Пустая строка нужна чтобы в евент что-то попало(он обязателен)
+            self.unbind_class('Tk','<Enter>')                   # Отвязываем событие при наведении мыши на окно
             self.unbind_class('Tk','<Leave>')
-            self.combo_win.unbind('ComboboxSelected>>')    # Отвязываем событие выбора пунктов из сомбобокса
-            self.after_cancel(self.wheel)                   # Обрываем процесс, перезапускающий функцию получения данных от процессора данные из ядер
-            self.clear_win()                                # вызываем метод очищающий окно
+            self.combo_win.unbind('ComboboxSelected>>')         # Отвязываем событие выбора пунктов из сомбобокса
+            self.after_cancel(self.wheel)                       # Обрываем процесс, перезапускающий функцию получения данных от процессора данные из ядер
+            self.clear_win()                                    # вызываем метод очищающий окно
             self.update()
             self.make_minimal_win()
 
 
-    def make_full_win(self):            # Заполнение ркна при переходе в фул режим
-        self.after_cancel(self.wheel)   # Остановили процесс обновления получения значений от cpu
-        self.clear_win()                # очистили окно от виджетов
+    def make_full_win(self):                                    # Заполнение ркна при переходе в фул режим
+        self.after_cancel(self.wheel)                           # Остановили процесс обновления получения значений от cpu
+        self.clear_win()                                        # очистили окно от виджетов
         self.update()
-        self.run_set_ui()               # Запускаем прорисовку главного окна
+        self.run_set_ui()                                       # Запускаем прорисовку главного окна
 
 
-    def app_exit(self):             # Метод класса
+    def app_exit(self):                                         # Метод класса
         self.quit()
-        sys.exit()                  #Убить процесс
+        sys.exit()                                              # Убить процесс
 
 
 
 if __name__ == '__main__':
-    root = Application ()               # Создание базового окна в функцоинальном стиле
+    root = Application ()                                       # Создание базового окна в функцоинальном стиле
     root.mainloop()
--- proccess.py
+++ proccess.py
@@ -6,11 +6,11 @@ import psutil as pt
 class CpuBar:
 
     def __init__(self):
-        self.cpu_count = pt.cpu_count(logical=False)         #Выводит количество ядер у процессора (без логических ядер т.е. без потоков)
-        self.cpu_count_logical = pt.cpu_count()              #Выводит количество потоков, на каждоя ядре 2 потока.
+        self.cpu_count = pt.cpu_count(logical=False)         # Выводит количество ядер у процессора (без логических ядер т.е. без потоков)
+        self.cpu_count_logical = pt.cpu_count()              # Выводит количество потоков, на каждоя ядре 2 потока.
 
 
-    def cpu_percent_return(self):                            #Возвращает загрузку процессоров в процентах   (список из 4 значений каждый на ядро)
+    def cpu_percent_return(self):                            # Возвращает загрузку процессоров в процентах   (список из 4 значений каждый на ядро)
         return pt.cpu_percent(percpu=True)                   # Percpu-true - возвращает значение по каждому потоку а не общее значение
 
     def cpu_one_return(self):                                # считывает показатели с ядер
@@ -18,7 +18,7 @@ class CpuBar:
 
 
     def ram_usage(self):                                     # Возвращает использование памяти
-        return pt.virtual_memory()                           #Возвращает именнованный кортеж
+        return pt.virtual_memory()                           # Возвращает именнованный кортеж
 #svmem(total=12438216704, available=5186166784, percent=58.3, used=6606327808, free=1049608192, active=7835869184, inactive=2783125504, buffers=87822336, cached=4694458368, shared=315187200, slab=552849408)
 
 
